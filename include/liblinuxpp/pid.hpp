c857e64a5ec20af409b6c10909a9530c62117bed
Nick Guiffrida <goodfella005@gmail.com>
2018-09-29 13:47:29 -0700

    Reimplement pid as an int_descriptor alias

--- include/liblinuxpp/pid.hpp
+++ include/liblinuxpp/pid.hpp
@@ -4,76 +4,24 @@
 #include <sys/types.h>
 #include <utility>
 
+#include <liblinuxpp/int_descriptor.hpp>
+
 namespace linuxpp
 {
     constexpr pid_t invalid_pid = -1;
-    class pid
+
+    namespace detail
     {
-        public:
+        struct pid_tag;
+    }
 
-        constexpr pid(const pid_t value = linuxpp::invalid_pid) noexcept;
-
-        pid(const pid& other) = default;
-        pid & operator=(const pid& other) = default;
-
-        pid(pid&& other) noexcept;
-        pid& operator=(pid&& other) noexcept;
-
-        pid_t get() const noexcept;
-
-        void swap(pid& other) noexcept;
-        void reset(const pid_t other = invalid_pid) noexcept;
-        pid_t release() noexcept;
-
-        explicit
-        operator bool() const noexcept;
-
-        private:
-
-        pid_t value_;
+    template<>
+    struct int_descriptor_traits<detail::pid_tag>
+    {
+        static constexpr pid_t invalid_value = invalid_pid;
     };
 
-    inline constexpr pid::pid(const pid_t value) noexcept:
-                                                  value_(value)
-    {}
-
-    inline pid::pid(pid&& other) noexcept:
-                                  value_(other.value_)
-    {}
-
-    inline pid& pid::operator=(pid&& other) noexcept
-    {
-        this->value_ = other.value_;
-        other.value_ = invalid_pid;
-        return *this;
-    }
-
-    inline pid_t pid::get() const noexcept
-    {
-        return this->value_;
-    }
-
-    inline void pid::swap(pid& other) noexcept
-    {
-        std::swap(this->value_, other.value_);
-    }
-
-    inline void pid::reset(const pid_t other) noexcept
-    {
-        this->value_ = other;
-    }
-
-    inline pid_t pid::release() noexcept
-    {
-        const pid_t ret = this->value_;
-        this->value_ = linuxpp::invalid_pid;
-        return ret;
-    }
-
-    inline pid::operator bool() const noexcept
-    {
-        return this->value_ != linuxpp::invalid_pid;
-    }
+    using pid = int_descriptor<detail::pid_tag, pid_t>;
 }
 
 #endif
