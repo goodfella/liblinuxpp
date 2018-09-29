1ce786b6b065ab04931f77126b28f287ddec095a
Nick Guiffrida <goodfella005@gmail.com>
2018-09-29 13:39:59 -0700

    [int_descriptor] Allow specification of value type

--- include/liblinuxpp/int_descriptor.hpp
+++ include/liblinuxpp/int_descriptor.hpp
@@ -8,7 +8,7 @@
 
 namespace linuxpp
 {
-    template <class T>
+    template <class Tag, class T = int>
     struct int_descriptor_traits;
 
     struct int_descriptor_default_traits
@@ -17,16 +17,17 @@ namespace linuxpp
     };
 
     /// Represents an integer based descriptor
-    template <class T>
+    template <class Tag, class T = int>
     class int_descriptor
     {
         public:
 
-        using type_traits = std::conditional_t<ndgpp::is_class_defined<int_descriptor_traits<T>>::value,
-                                               int_descriptor_traits<T>,
+        using type_traits = std::conditional_t<ndgpp::is_class_defined<int_descriptor_traits<Tag, T>>::value,
+                                               int_descriptor_traits<Tag, T>,
                                                int_descriptor_default_traits>;
+        using value_type = T;
 
-        constexpr int_descriptor(const int descriptor = type_traits::invalid_value) noexcept;
+        constexpr int_descriptor(const T descriptor = type_traits::invalid_value) noexcept;
 
         int_descriptor(const int_descriptor&) = default;
         int_descriptor& operator=(const int_descriptor&) = default;
@@ -34,75 +35,83 @@ namespace linuxpp
         int_descriptor(int_descriptor&&) noexcept;
         int_descriptor& operator=(int_descriptor&&) noexcept;
 
-        int get() const noexcept;
+        T get() const noexcept;
 
         void swap(int_descriptor& other) noexcept;
 
-        void reset(int fd = type_traits::invalid_value) noexcept;
+        void reset(T fd = type_traits::invalid_value) noexcept;
+
+        explicit operator bool() const noexcept;
 
         private:
 
         int descriptor_;
     };
 
-    template <class T>
-    inline void swap(int_descriptor<T>& lhs, int_descriptor<T>& rhs) noexcept
+    template <class Tag, class T>
+    inline void swap(int_descriptor<Tag, T>& lhs, int_descriptor<Tag, T>& rhs) noexcept
     {
         lhs.swap(rhs);
     }
 
-    template <class T>
-    inline constexpr int_descriptor<T>::int_descriptor(const int descriptor) noexcept:
+    template <class Tag, class T>
+    inline constexpr int_descriptor<Tag, T>::int_descriptor(const T descriptor) noexcept:
         descriptor_(descriptor)
     {}
 
-    template <class T>
-    inline int_descriptor<T>::int_descriptor(int_descriptor<T>&& other) noexcept:
+    template <class Tag, class T>
+    inline int_descriptor<Tag, T>::int_descriptor(int_descriptor&& other) noexcept:
         descriptor_(other.descriptor_)
     {
         other.descriptor_ = type_traits::invalid_value;
     }
 
-    template <class T>
-    inline int_descriptor<T>& int_descriptor<T>::operator=(int_descriptor<T>&& other) noexcept
+    template <class Tag, class T>
+    inline int_descriptor<Tag, T>& int_descriptor<Tag, T>::operator=(int_descriptor&& other) noexcept
     {
         this->descriptor_ = other.descriptor_;
         other.descriptor_ = type_traits::invalid_value;
         return *this;
     }
 
-    template <class T>
-    inline void int_descriptor<T>::swap(int_descriptor<T>& other) noexcept
+    template <class Tag, class T>
+    inline void int_descriptor<Tag, T>::swap(int_descriptor& other) noexcept
     {
         std::swap(this->descriptor_, other.descriptor_);
     }
 
-    template <class T>
-    inline void int_descriptor<T>::reset(const int descriptor) noexcept
+    template <class Tag, class T>
+    inline void int_descriptor<Tag, T>::reset(const T descriptor) noexcept
     {
         this->descriptor_ = descriptor;
     }
 
-    template <class T>
-    inline int int_descriptor<T>::get() const noexcept
+    template <class Tag, class T>
+    inline T int_descriptor<Tag, T>::get() const noexcept
     {
         return this->descriptor_;
     }
 
-    template <class T>
-    inline bool operator==(const int_descriptor<T> lhs, const int_descriptor<T> rhs)
+    template <class Tag, class T>
+    int_descriptor<Tag, T>::operator bool() const noexcept
+    {
+        return this->descriptor_ != type_traits::invalid_value;
+    }
+
+    template <class Tag, class T>
+    inline bool operator==(const int_descriptor<Tag, T> lhs, const int_descriptor<Tag, T> rhs)
     {
         return lhs.get() == rhs.get();
     }
 
-    template <class T>
-    inline bool operator!=(const int_descriptor<T> lhs, const int_descriptor<T> rhs)
+    template <class Tag, class T>
+    inline bool operator!=(const int_descriptor<Tag, T> lhs, const int_descriptor<Tag, T> rhs)
     {
         return !(lhs == rhs);
     }
 
-    template <class T>
-    inline bool operator<(const int_descriptor<T> lhs, const int_descriptor<T> rhs)
+    template <class Tag, class T>
+    inline bool operator<(const int_descriptor<Tag, T> lhs, const int_descriptor<Tag, T> rhs)
     {
         return lhs.get() < rhs.get();
     }
