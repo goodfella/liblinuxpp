bf3512b4557bc5fba63d31607302e847295809c1
Nick Guiffrida <goodfella005@gmail.com>
2018-09-29 20:05:44 -0700

    Add epoll class for managing epoll file descriptors

---
+++ include/liblinuxpp/epoll.hpp
@@ -0,0 +1,139 @@
+#ifndef LIBLINUXPP_EPOLL_HPP
+#define LIBLINUXPP_EPOLL_HPP
+
+#include <sys/epoll.h>
+
+#include <cstdint>
+
+#include <chrono>
+#include <tuple>
+#include <vector>
+
+#include <liblinuxpp/unique_fd.hpp>
+
+namespace linuxpp
+{
+    /** Manages an epoll file descriptor
+     *
+     *  @par Copy Semantics Non-copyable
+     */
+    class epoll
+    {
+        /// Constructs an epoll object using EPOLL_CLOEXEC
+        epoll();
+
+        epoll(const epoll&) = delete;
+        epoll& operator= (const epoll&) = delete;
+
+        /** Adds a file descriptor to the epoll fd
+         *
+         *  Assigns epoll_event::fd to fd
+         *
+         *  @throws ndgpp::error<std::system_error> if an error is encountered
+         */
+        void add(const int fd, const uint32_t events);
+
+        /** Adds a file descriptor to the epoll fd
+         *
+         *  Assigns epoll_event::ptr to context
+         *
+         *  @throws ndgpp::error<std::system_error> if an error is encountered
+         */
+        void add(const int fd, const uint32_t events, void * const context);
+
+        /** Adds a file descriptor to the epoll fd
+         *
+         *  Assigns epoll_event::u32 to context
+         *
+         *  @throws ndgpp::error<std::system_error> if an error is encountered
+         */
+        void add(const int fd, const uint32_t events, const uint32_t context);
+
+        /** Adds a file descriptor to the epoll fd
+         *
+         *  Assigns epoll_event::u64 to context
+         *
+         *  @throws ndgpp::error<std::system_error> if an error is encountered
+         */
+        void add(const int fd, const uint32_t events, const uint64_t context);
+
+        /** Deletes fd from the epoll fd
+         *
+         *  @throws ndgpp::error<std::system_error> if an error is encountered
+         */
+        void del(const int fd);
+
+        /** Modifies a file descriptor's epoll_event object
+         *
+         *  Assigns epoll_event::fd to fd
+         *
+         *  @throws ndgpp::error<std::system_error> if an error is encountered
+         */
+        void mod(const int fd, const uint32_t events);
+
+        /** Modifies a file descriptor's epoll_event object
+         *
+         *  Assigns epoll_event::ptr to context
+         *
+         *  @throws ndgpp::error<std::system_error> if an error is encountered
+         */
+        void mod(const int fd, const uint32_t events, void * const context);
+
+        /** Modifies a file descriptor's epoll_event object
+         *
+         *  Assigns epoll_event::u32 to context
+         *
+         *  @throws ndgpp::error<std::system_error> if an error is encountered
+         */
+        void mod(const int fd, const uint32_t events, const uint32_t context);
+
+        /** Modifies a file descriptor's epoll_event object
+         *
+         *  Assigns epoll_event::u64 to context
+         *
+         *  @throws ndgpp::error<std::system_error> if an error is encountered
+         */
+        void mod(const int fd, const uint32_t events, const uint64_t context);
+
+        /** Waits an unlimited amount of time for an event on the monitored file descriptors
+         *
+         *  @throws ndgpp::error<std::system_error> if an error is encountered
+         */
+        std::vector<epoll_event> wait();
+
+        /** Waits for an event on the monitored file descriptors
+         *
+         *  @throws ndgpp::error<std::system_error> if an error is encountered
+         */
+        std::vector<epoll_event> wait(const std::chrono::milliseconds timeout);
+
+        /** Waits an unlimited amount of time for an event on the monitored file descriptors
+         *
+         *  @throws ndgpp::error<std::system_error> if an error is encountered
+         */
+        void wait(std::vector<epoll_event>& events);
+
+        /** Waits for an event on the monitored file descriptors
+         *
+         *  @throws ndgpp::error<std::system_error> if an error is encountered
+         */
+        void wait(std::vector<epoll_event>& events,
+                  const std::chrono::milliseconds timeout);
+
+        private:
+
+        void add(const int fd, epoll_event event);
+        void mod(const int fd, epoll_event event);
+        void wait(std::vector<epoll_event>& events, const int timeout);
+
+        enum members
+        {
+            epoll_fd,
+            size_events
+        };
+
+        std::tuple<linuxpp::unique_fd<>, std::size_t> members_;
+    };
+}
+
+#endif
---
+++ src/epoll.cpp
@@ -0,0 +1,171 @@
+#include <cerrno>
+#include <system_error>
+
+#include <libndgpp/error.hpp>
+#include <liblinuxpp/epoll.hpp>
+
+inline epoll_event make_epoll_event(const int fd, const uint32_t events)
+{
+    epoll_event event;
+    event.events = events;
+    event.data.fd = fd;
+    return event;
+}
+
+inline epoll_event make_epoll_event(const int fd, const uint32_t events, void * const context)
+{
+    epoll_event event;
+    event.events = events;
+    event.data.ptr = context;
+    return event;
+}
+
+inline epoll_event make_epoll_event(const int fd, const uint32_t events, const uint32_t context)
+{
+    epoll_event event;
+    event.events = events;
+    event.data.u32 = context;
+    return event;
+}
+
+inline epoll_event make_epoll_event(const int fd, const uint32_t events, const uint64_t context)
+{
+    epoll_event event;
+    event.events = events;
+    event.data.u64 = context;
+    return event;
+}
+
+linuxpp::epoll::epoll():
+    members_(::epoll_create1(EPOLL_CLOEXEC), 0)
+{
+    if (!std::get<epoll_fd>(this->members_))
+    {
+        throw ndgpp_error(std::system_error,
+                          std::error_code{errno, std::system_category()},
+                          "epoll_create failed");
+    }
+}
+
+void linuxpp::epoll::add(const int fd, const uint32_t events)
+{
+    this->add(fd, make_epoll_event(fd, events));
+}
+
+void linuxpp::epoll::add(const int fd, const uint32_t events, void * const context)
+{
+    this->add(fd, make_epoll_event(fd, events, context));
+}
+
+void linuxpp::epoll::add(const int fd, const uint32_t events, const uint32_t context)
+{
+    this->add(fd, make_epoll_event(fd, events, context));
+}
+
+void linuxpp::epoll::add(const int fd, const uint32_t events, const uint64_t context)
+{
+    this->add(fd, make_epoll_event(fd, events, context));
+}
+
+void linuxpp::epoll::del(const int fd)
+{
+    const int ret = ::epoll_ctl(std::get<epoll_fd>(this->members_).get(),  EPOLL_CTL_DEL, fd, nullptr);
+    if (ret != 0)
+    {
+        throw ndgpp_error(std::system_error,
+                          std::error_code{errno, std::system_category()},
+                          "epoll_ctl::EPOLL_CTL_DEL failed");
+    }
+
+    --std::get<size_events>(this->members_);
+}
+
+
+void linuxpp::epoll::mod(const int fd, const uint32_t events)
+{
+    this->mod(fd, make_epoll_event(fd, events));
+}
+
+void linuxpp::epoll::mod(const int fd, const uint32_t events, void * const context)
+{
+    this->mod(fd, make_epoll_event(fd, events, context));
+}
+
+void linuxpp::epoll::mod(const int fd, const uint32_t events, const uint32_t context)
+{
+    this->mod(fd, make_epoll_event(fd, events, context));
+}
+
+void linuxpp::epoll::mod(const int fd, const uint32_t events, const uint64_t context)
+{
+    this->mod(fd, make_epoll_event(fd, events, context));
+}
+
+void linuxpp::epoll::wait(std::vector<epoll_event>& events)
+{
+    this->wait(events, -1);
+}
+
+void linuxpp::epoll::wait(std::vector<epoll_event>& events, const std::chrono::milliseconds timeout)
+{
+    this->wait(events, timeout.count());
+}
+
+std::vector<epoll_event> linuxpp::epoll::wait()
+{
+    std::vector<epoll_event> events;
+    this->wait(events, -1);
+    return events;
+}
+
+std::vector<epoll_event> linuxpp::epoll::wait(const std::chrono::milliseconds timeout)
+{
+    std::vector<epoll_event> events;
+    this->wait(events, timeout.count());
+    return events;
+}
+
+void linuxpp::epoll::add(const int fd, epoll_event event)
+{
+    const int ret = ::epoll_ctl(std::get<epoll_fd>(this->members_).get(),
+                                EPOLL_CTL_ADD,
+                                fd,
+                                &event);
+    if (ret == -1)
+    {
+        throw ndgpp_error(std::system_error,
+                          std::error_code{errno, std::system_category()},
+                          "epoll_ctl::EPOLL_CTL_ADD failed");
+    }
+
+    ++std::get<size_events>(this->members_);
+}
+
+void linuxpp::epoll::mod(const int fd, epoll_event event)
+{
+    const int ret = ::epoll_ctl(std::get<epoll_fd>(this->members_).get(),
+                                EPOLL_CTL_MOD,
+                                fd,
+                                &event);
+    if (ret == -1)
+    {
+        throw ndgpp_error(std::system_error,
+                          std::error_code{errno, std::system_category()},
+                          "epoll_ctl::EPOLL_CTL_MOD failed");
+    }
+}
+
+void linuxpp::epoll::wait(std::vector<epoll_event>& events, const int timeout)
+{
+    events.resize(std::get<size_events>(this->members_));
+    const int ret = ::epoll_wait(std::get<epoll_fd>(this->members_).get(),
+                                 events.data(),
+                                 events.size(),
+                                 timeout);
+    if (ret == -1)
+    {
+        throw ndgpp_error(std::system_error,
+                          std::error_code{errno, std::system_category()},
+                          "epoll_wait failed");
+    }
+}
