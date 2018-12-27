290dc69e2ea17baf0a62c8cff4e26e6deb88143e
Nick Guiffrida <goodfella005@gmail.com>
2018-12-26 20:15:40 -0800

    net::make_sockaddr: Convert ipv4_address::uint32 to network byte order

--- include/liblinuxpp/net/sockaddr.hpp
+++ include/liblinuxpp/net/sockaddr.hpp
@@ -20,8 +20,13 @@ namespace net {
      *
      *  @{
      */
+
+    /// Returns a sockaddr_in that represents the address and port provided
     struct ::sockaddr_in make_sockaddr(const ndgpp::net::ipv4_address addr,
                                        const ndgpp::net::port port);
+
+    /// Returns a sockaddr_in that represents the address provided and a value of 0 for the port
+    struct ::sockaddr_in make_sockaddr(const ndgpp::net::ipv4_address addr);
 
     /// @}
 }}
--- src/net/sockaddr.cpp
+++ src/net/sockaddr.cpp
@@ -8,7 +8,16 @@ struct sockaddr_in linuxpp::net::make_sockaddr(const ndgpp::net::ipv4_address ad
     sockaddr_in sockaddr = {};
     sockaddr.sin_family = AF_INET;
     sockaddr.sin_port = htons(port.value());
-    sockaddr.sin_addr.s_addr = address.to_uint32();
+    sockaddr.sin_addr.s_addr = htonl(address.to_uint32());
+
+    return sockaddr;
+}
+
+struct sockaddr_in linuxpp::net::make_sockaddr(const ndgpp::net::ipv4_address addr)
+{
+    sockaddr_in sockaddr = {};
+    sockaddr.sin_family = AF_INET;
+    sockaddr.sin_addr.s_addr = htonl(addr.to_uint32());
 
     return sockaddr;
 }
