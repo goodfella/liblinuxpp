3ed2575e71e07497bad19abd12c81ce5bd797455
Nick Guiffrida <goodfella005@gmail.com>
2018-12-26 20:26:18 -0800

    net::ipv4_address: Rename inaddr_localhost to inaddr_loopback

--- include/liblinuxpp/net/ipv4_address.hpp
+++ include/liblinuxpp/net/ipv4_address.hpp
@@ -1,19 +1,18 @@
 #ifndef LIBLINUXPP_NET_BIND_HPP
 #define LIBLINUXPP_NET_BIND_HPP
 
+#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 
 #include <libndgpp/net/ipv4_address.hpp>
 #include <libndgpp/net/port.hpp>
-#include <libndgpp/net/protocols.hpp>
-
 
 namespace linuxpp {
 namespace net {
 
     static constexpr ndgpp::net::ipv4_address inaddr_any {INADDR_ANY};
-    static constexpr ndgpp::net::ipv4_address inaddr_localhost {INADDR_LOOPBACK};
+    static constexpr ndgpp::net::ipv4_address inaddr_loopback {INADDR_LOOPBACK};
 }}
 
 #endif
