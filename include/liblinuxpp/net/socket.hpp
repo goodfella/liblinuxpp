3c7f1050299e87dfd0b39e733dee99fdbfd3d4c4
Nick Guiffrida <goodfella005@gmail.com>
2018-12-28 11:26:00 -0800

    net/socket.hpp: Add getsockname functions

--- include/liblinuxpp/net/socket.hpp
+++ include/liblinuxpp/net/socket.hpp
@@ -1,6 +1,12 @@
 #ifndef LIBLINUXPP_SOCKET_HPP
 #define LIBLINUXPP_SOCKET_HPP
 
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <sys/socket.h>
+
+#include <tuple>
+
 #include <liblinuxpp/unique_fd.hpp>
 
 #include <libndgpp/net/ipv4_address.hpp>
@@ -23,6 +29,29 @@ namespace net
      *  @throws ndgpp::error<std::system_error> if an error occurs creating the socket
      */
     linuxpp::unique_fd<> socket(const int domain, const int type, const int protocol);
+
+
+    /** @defgroup net::getsockname net::getsockname
+     *
+     *  The net::getsockname
+     *
+     *  @param sd The socket descriptor
+     *  @param sockaddr [out] The sockaddr_in to write the name information to
+     *
+     *  @{
+     */
+    void getsockname(const int sd, struct ::sockaddr_in & sockaddr);
+
+    /// @}
+
+
+    /** Returns IPv4 name information for a socket
+     *
+     *  @param sd The socket descriptor
+     *
+     *  @throws ndgpp::error<std::invalid_argument> if the socket is not bound to an IPv4 address
+     */
+    std::tuple<ndgpp::net::ipv4_address, ndgpp::net::port> getsockname_ipv4(const int sd);
 }
 }
 
--- src/net/socket.cpp
+++ src/net/socket.cpp
@@ -1,3 +1,4 @@
+#include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 
@@ -19,4 +20,24 @@ linuxpp::unique_fd<> linuxpp::net::socket(const int domain, const int type, cons
     }
 
     return fd;
+}
+
+void linuxpp::net::getsockname(const int sd, struct ::sockaddr_in & sockaddr)
+{
+    socklen_t socklen = sizeof(sockaddr);
+    const int ret = ::getsockname(sd, reinterpret_cast<struct ::sockaddr *>(&sockaddr), &socklen);
+    if (ret == -1)
+    {
+        throw ndgpp_error(std::system_error,
+                          std::error_code (errno, std::system_category()),
+                          "getsockname failed");
+    }
+}
+
+std::tuple<ndgpp::net::ipv4_address, ndgpp::net::port> linuxpp::net::getsockname_ipv4(const int sd)
+{
+    struct ::sockaddr_in sockaddr;
+    linuxpp::net::getsockname(sd, sockaddr);
+
+    return std::make_tuple(ndgpp::net::ipv4_address{ntohl(sockaddr.sin_addr.s_addr)}, ntohs(sockaddr.sin_port));
 }
